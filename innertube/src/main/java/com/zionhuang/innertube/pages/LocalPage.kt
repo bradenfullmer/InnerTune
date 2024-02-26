0bb32bfec17f944fa8d761a7a100ed3518f9d466
Braden Fullmer <46699133+bradenfullmer@users.noreply.github.com>
2024-02-26 08:10:19 -0700

    Initial Screen Creation
    Trying to solve the errors with setting up a new screen in kotlin

--- innertube/src/main/java/com/zionhuang/innertube/pages/LocalPage.kt
+++ innertube/src/main/java/com/zionhuang/innertube/pages/LocalPage.kt
@@ -9,9 +9,6 @@ class TestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
-
-        // Your initialization code goes here
     }
 }
 /*data class LocalPage (
